657850f142290bb53a93bad4b2a92a775db46e73
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-27 14:33:09 +0000

    BROKERS: Provider

--- LondonFhirService.Core/Brokers/Storages/Sql/StorageBroker.Provider.cs
+++ LondonFhirService.Core/Brokers/Storages/Sql/StorageBroker.Provider.cs
@@ -1,7 +1,10 @@
-// ---------------------------------------------------------
+﻿// ---------------------------------------------------------
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using LondonFhirService.Core.Models.Foundations.Providers;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +13,20 @@ namespace LondonFhirService.Core.Brokers.Storages.Sql
     public partial class StorageBroker
     {
         public DbSet<Provider> Providers { get; set; }
+
+        public async ValueTask<Provider> InsertProviderAsync(Provider provider) =>
+            await InsertAsync(provider);
+
+        public async ValueTask<IQueryable<Provider>> SelectAllProvidersAsync() =>
+            await SelectAllAsync<Provider>();
+
+        public async ValueTask<Provider> SelectProviderByIdAsync(Guid providerId) =>
+            await SelectAsync<Provider>(providerId);
+
+        public async ValueTask<Provider> UpdateProviderAsync(Provider provider) =>
+            await UpdateAsync(provider);
+
+        public async ValueTask<Provider> DeleteProviderAsync(Provider provider) =>
+            await DeleteAsync(provider);
     }
 }
