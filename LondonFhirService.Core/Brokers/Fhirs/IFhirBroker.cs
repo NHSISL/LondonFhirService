45fe9c6063bc82ab7ce7e7c5a249a9acd1cb80cf
Christo du Toit <christo@dutoit.co.uk>
2025-10-27 17:57:31 +0000

    BROKERS: Fhir Broker

---
+++ LondonFhirService.Core/Brokers/Fhirs/IFhirBroker.cs
@@ -0,0 +1,10 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace LondonFhirService.Core.Brokers.Fhirs
+{
+    public interface IFhirBroker
+    {
+    }
+}
