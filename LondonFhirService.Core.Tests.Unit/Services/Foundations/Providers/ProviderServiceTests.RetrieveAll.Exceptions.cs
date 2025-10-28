d44468891fa6101c200b020ff2afa83cca64038c
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-28 15:41:32 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/Providers/ProviderServiceTests.RetrieveAll.Exceptions.cs
@@ -0,0 +1,63 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.Providers;
+using LondonFhirService.Core.Models.Foundations.Providers.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.Providers
+{
+    public partial class ProviderServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageProviderServiceException =
+                new FailedStorageProviderServiceException(
+                    message: "Failed provider storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedProviderServiceDependencyException =
+                new ProviderServiceDependencyException(
+                    message: "Provider dependency error occurred, contact support.",
+                    innerException: failedStorageProviderServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllProvidersAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IQueryable<Provider>> retrieveAllProvidersTask = this.providerService.RetrieveAllProvidersAsync();
+
+            ProviderServiceDependencyException actualProviderServiceDependencyException =
+                await Assert.ThrowsAsync<ProviderServiceDependencyException>(
+                    retrieveAllProvidersTask.AsTask);
+
+            // then
+            actualProviderServiceDependencyException.Should()
+                .BeEquivalentTo(expectedProviderServiceDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllProvidersAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedProviderServiceDependencyException))),
+                        Times.Once);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
