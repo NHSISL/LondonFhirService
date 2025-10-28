b7e21c818cc2a64d1dbb77f9aff1455852771ef8
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-28 11:24:44 +0000

    ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/Providers/ProviderServiceTests.Modify.Exceptions.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/Providers/ProviderServiceTests.Modify.Exceptions.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using LondonFhirService.Core.Models.Foundations.Providers;
 using LondonFhirService.Core.Models.Foundations.Providers.Exceptions;
@@ -55,6 +56,77 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.Providers
                 broker.LogCriticalAsync(It.Is(SameExceptionAs(
                     expectedProviderServiceDependencyException))),
                         Times.Once);
+
+            this.securityAuditBrokerMock.Verify(broker =>
+                broker.GetUserIdAsync(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectProviderByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.securityAuditBrokerMock.Verify(broker => broker
+                .EnsureAddAuditValuesRemainsUnchangedOnModifyAsync(It.IsAny<Provider>(), It.IsAny<Provider>()),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateProviderAsync(It.IsAny<Provider>()),
+                    Times.Never);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync()
+        {
+            // given
+            Provider someProvider = CreateRandomProvider();
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+
+            var foreignKeyConstraintConflictException =
+                new ForeignKeyConstraintConflictException(exceptionMessage);
+
+            var invalidProviderReferenceException =
+                new InvalidReferenceProviderServiceException(
+                    message: "Invalid provider reference error occurred.",
+                    innerException: foreignKeyConstraintConflictException);
+
+            ProviderServiceDependencyValidationException expectedProviderServiceDependencyValidationException =
+                new ProviderServiceDependencyValidationException(
+                    message: "Provider dependency validation occurred, please try again.",
+                    innerException: invalidProviderReferenceException);
+
+            this.securityAuditBrokerMock.Setup(broker =>
+                broker.ApplyModifyAuditValuesAsync(It.IsAny<Provider>()))
+                    .ThrowsAsync(foreignKeyConstraintConflictException);
+
+            // when
+            ValueTask<Provider> modifyProviderTask =
+                this.providerService.ModifyProviderAsync(someProvider);
+
+            ProviderServiceDependencyValidationException actualProviderServiceDependencyValidationException =
+                await Assert.ThrowsAsync<ProviderServiceDependencyValidationException>(
+                    modifyProviderTask.AsTask);
+
+            // then
+            actualProviderServiceDependencyValidationException.Should()
+                .BeEquivalentTo(expectedProviderServiceDependencyValidationException);
+
+            this.securityAuditBrokerMock.Verify(broker =>
+                broker.ApplyModifyAuditValuesAsync(It.IsAny<Provider>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(expectedProviderServiceDependencyValidationException))),
+                    Times.Once);
 
             this.securityAuditBrokerMock.Verify(broker =>
                 broker.GetUserIdAsync(),
