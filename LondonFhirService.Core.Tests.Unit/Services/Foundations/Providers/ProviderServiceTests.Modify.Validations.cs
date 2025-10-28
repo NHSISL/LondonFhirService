a673e04d68391394294445f5df56391963292aad
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-28 10:58:22 +0000

    ShouldThrowValidationExceptionOnModifyIfProviderIsNullAndLogItAsync -> PASS

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/Providers/ProviderServiceTests.Modify.Validations.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.Providers;
+using LondonFhirService.Core.Models.Foundations.Providers.Exceptions;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.Providers
+{
+    public partial class ProviderServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfProviderIsNullAndLogItAsync()
+        {
+            // given
+            Provider nullProvider = null;
+            var nullProviderServiceException = new NullProviderServiceException(message: "Provider is null.");
+
+            var expectedProviderServiceValidationException =
+                new ProviderServiceValidationException(
+                    message: "Provider validation errors occurred, please try again.",
+                    innerException: nullProviderServiceException);
+
+            this.securityAuditBrokerMock.Setup(broker =>
+                broker.ApplyModifyAuditValuesAsync(nullProvider))
+                    .ReturnsAsync(nullProvider);
+
+            // when
+            ValueTask<Provider> modifyProviderTask =
+                this.providerService.ModifyProviderAsync(nullProvider);
+
+            ProviderServiceValidationException actualProviderServiceValidationException =
+                await Assert.ThrowsAsync<ProviderServiceValidationException>(
+                    modifyProviderTask.AsTask);
+
+            // then
+            actualProviderServiceValidationException.Should()
+                .BeEquivalentTo(expectedProviderServiceValidationException);
+
+            this.securityAuditBrokerMock.Verify(broker =>
+                broker.ApplyModifyAuditValuesAsync(nullProvider),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedProviderServiceValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateProviderAsync(It.IsAny<Provider>()),
+                    Times.Never);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
