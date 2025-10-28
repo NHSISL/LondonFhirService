b5441131d8ffc7f1eef6ce141319f017086ec76a
Joe Owens <joe.owens@answerdigital.com>
2025-10-28 14:25:59 +0000

    Initial logic for Patient service GetStructuredRecord

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/Patients/PatientServiceTests.GetStructuredPatient.Logic.cs
@@ -0,0 +1,77 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using FluentAssertions;
+using Force.DeepCloner;
+using Hl7.Fhir.Model;
+using Moq;
+using Task = System.Threading.Tasks.Task;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.Patients
+{
+    public partial class PatientServiceTests
+    {
+        [Fact]
+        public async Task ShouldGetStructuredPatient()
+        {
+            // given
+            List<string> randomProviderNames = new List<string>
+            {
+                GetRandomString(),
+                GetRandomString()
+            };
+
+            List<string> inputProviderNames = randomProviderNames.DeepClone();
+            Bundle randomBundle = CreateRandomBundle();
+            Bundle outputBundle = randomBundle.DeepClone();
+            string randomNhsNumber = GetRandomString();
+            string inputNhsNumber = randomNhsNumber;
+
+            foreach (string providerName in inputProviderNames)
+            {
+                this.fhirBrokerMock.Setup(broker =>
+                    broker.Patients(providerName).Everything(
+                        inputNhsNumber,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null))
+                        .Returns(outputBundle);
+            }
+
+            List<Bundle> expectedBundles = new List<Bundle>
+            {
+                outputBundle,
+                outputBundle
+            };
+
+            // when
+            List<Bundle> actualBundles =
+                await this.patientService.GetStructuredRecord(
+                    providers: inputProviderNames,
+                    nhsNumber: inputNhsNumber,
+                    cancellationToken: default);
+
+            // then
+            actualBundles.Should().BeEquivalentTo(expectedBundles);
+
+            foreach (string providerName in inputProviderNames)
+            {
+                this.fhirBrokerMock.Verify(broker =>
+                    broker.Patients(providerName).Everything(
+                        inputNhsNumber,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null),
+                            Times.Once);
+            }
+
+            this.fhirBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/Patients/PatientServiceTests.cs
@@ -0,0 +1,90 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Hl7.Fhir.Model;
+using LondonFhirService.Core.Brokers.Fhirs;
+using LondonFhirService.Core.Models.Foundations.Patients;
+using LondonFhirService.Core.Services.Foundations.Patients;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.Patients
+{
+    public partial class PatientServiceTests
+    {
+        private readonly Mock<IFhirBroker> fhirBrokerMock;
+        private readonly PatientServiceConfig patientServiceConfig;
+        private readonly IPatientService patientService;
+
+        public PatientServiceTests()
+        {
+            this.fhirBrokerMock = new Mock<IFhirBroker>();
+
+            this.patientServiceConfig = new PatientServiceConfig
+            {
+                MaxProviderWaitTimeSeconds = 60
+            };
+
+            this.patientService = new PatientService(
+                fhirBroker: this.fhirBrokerMock.Object,
+                patientServiceConfig: this.patientServiceConfig);
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static Patient CreateRandomPatient()
+        {
+            var patient = new Patient();
+
+            var nameFiller = new Filler<HumanName>();
+            nameFiller.Setup()
+                .OnProperty(n => n.Children).IgnoreIt()
+                .OnProperty(n => n.Extension).IgnoreIt()
+                .OnProperty(n => n.FamilyElement).IgnoreIt()
+                .OnProperty(n => n.GivenElement).IgnoreIt()
+                .OnProperty(n => n.NamedChildren).IgnoreIt()
+                .OnProperty(n => n.Period).IgnoreIt()
+                .OnProperty(n => n.PrefixElement).IgnoreIt()
+                .OnProperty(n => n.SuffixElement).IgnoreIt()
+                .OnProperty(n => n.TextElement).IgnoreIt()
+                .OnProperty(n => n.UseElement).IgnoreIt();
+
+            patient.Name = new List<HumanName> { nameFiller.Create() };
+            patient.Gender = AdministrativeGender.Male;
+            patient.BirthDate = GetRandomString();
+
+            return patient;
+        }
+
+        private Bundle CreateRandomBundle()
+        {
+            var bundle = new Bundle
+            {
+                Type = Bundle.BundleType.Searchset,
+                Total = 1,
+                Timestamp = DateTimeOffset.UtcNow
+            };
+
+            Patient patient = CreateRandomPatient();
+
+            bundle.Entry = new List<Bundle.EntryComponent>{
+                new Bundle.EntryComponent
+                {
+                    FullUrl = $"https://api.service.nhs.uk/personal-demographics/FHIR/R4/Patient/{patient.Id}",
+                    Search = new Bundle.SearchComponent { Score = 1 },
+                    Resource = patient
+                }
+            };
+
+
+            return bundle;
+        }
+    }
+}
