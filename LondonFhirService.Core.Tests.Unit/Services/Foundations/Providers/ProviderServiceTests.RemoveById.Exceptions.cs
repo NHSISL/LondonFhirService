a2cf7ddc745a25d0ec5e223efe4f4c68a5b6cd96
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-28 16:09:50 +0000

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/Providers/ProviderServiceTests.RemoveById.Exceptions.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/Providers/ProviderServiceTests.RemoveById.Exceptions.cs
@@ -2,11 +2,13 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LondonFhirService.Core.Models.Foundations.Providers;
 using LondonFhirService.Core.Models.Foundations.Providers.Exceptions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.Providers
@@ -61,6 +63,61 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.Providers
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someProviderId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedProviderServiceException =
+                new LockedProviderServiceException(
+                    message: "Locked provider record exception, please try again later",
+                    innerException: databaseUpdateConcurrencyException);
+
+            var expectedProviderServiceDependencyValidationException =
+                new ProviderServiceDependencyValidationException(
+                    message: "Provider dependency validation occurred, please try again.",
+                    innerException: lockedProviderServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectProviderByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<Provider> removeProviderByIdTask =
+                this.providerService.RemoveProviderByIdAsync(someProviderId);
+
+            ProviderServiceDependencyValidationException actualProviderServiceDependencyValidationException =
+                await Assert.ThrowsAsync<ProviderServiceDependencyValidationException>(
+                    removeProviderByIdTask.AsTask);
+
+            // then
+            actualProviderServiceDependencyValidationException.Should()
+                .BeEquivalentTo(expectedProviderServiceDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectProviderByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedProviderServiceDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteProviderAsync(It.IsAny<Provider>()),
                     Times.Never);
 
             this.securityAuditBrokerMock.VerifyNoOtherCalls();
