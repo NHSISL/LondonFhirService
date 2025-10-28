9754519f446455b1f4fb4eed9cc246e7465bb9b4
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-28 15:55:38 +0000

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> PASS

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/Providers/ProviderServiceTests.RetrieveById.Validations.cs
@@ -0,0 +1,62 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.Providers;
+using LondonFhirService.Core.Models.Foundations.Providers.Exceptions;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.Providers
+{
+    public partial class ProviderServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidProviderId = Guid.Empty;
+
+            var invalidProviderServiceException =
+                new InvalidProviderServiceException(
+                    message: "Invalid provider. Please correct the errors and try again.");
+
+            invalidProviderServiceException.AddData(
+                key: nameof(Provider.Id),
+                values: "Id is required");
+
+            var expectedProviderServiceValidationException =
+                new ProviderServiceValidationException(
+                    message: "Provider validation errors occurred, please try again.",
+                    innerException: invalidProviderServiceException);
+
+            // when
+            ValueTask<Provider> retrieveProviderByIdTask =
+                this.providerService.RetrieveProviderByIdAsync(invalidProviderId);
+
+            ProviderServiceValidationException actualProviderServiceValidationException =
+                await Assert.ThrowsAsync<ProviderServiceValidationException>(
+                    retrieveProviderByIdTask.AsTask);
+
+            // then
+            actualProviderServiceValidationException.Should()
+                .BeEquivalentTo(expectedProviderServiceValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(
+                    It.Is(SameExceptionAs(expectedProviderServiceValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectProviderByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
