24dc7ed3462b961cafc21fd4754eb8c257e4a336
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-28 15:55:03 +0000

    ShouldRetrieveProviderById

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/Providers/ProviderServiceTests.RetrieveById.Logic.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LondonFhirService.Core.Models.Foundations.Providers;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.Providers
+{
+    public partial class ProviderServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveProviderByIdAsync()
+        {
+            // given
+            Provider randomProvider = CreateRandomProvider();
+            Provider inputProvider = randomProvider;
+            Provider storageProvider = randomProvider;
+            Provider expectedProvider = storageProvider.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                    broker.SelectProviderByIdAsync(inputProvider.Id))
+                .ReturnsAsync(storageProvider);
+
+            // when
+            Provider actualProvider =
+                await this.providerService.RetrieveProviderByIdAsync(inputProvider.Id);
+
+            // then
+            actualProvider.Should().BeEquivalentTo(expectedProvider);
+
+            this.storageBrokerMock.Verify(broker =>
+                    broker.SelectProviderByIdAsync(inputProvider.Id),
+                Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
