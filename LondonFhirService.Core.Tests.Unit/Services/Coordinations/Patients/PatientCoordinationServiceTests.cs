7b581862d12ba806c02ae8fb11be2c0b3b855a38
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-29 12:16:54 +0000

    ShouldCallEverythingAsync -> FAIL

---
+++ LondonFhirService.Core.Tests.Unit/Services/Coordinations/Patients/PatientCoordinationServiceTests.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Coordinations.Patients
+{
+    internal class PatientCoordinationServiceTests
+    {
+    }
+}
---
+++ LondonFhirService.Core/Services/Coordinations/Patients/PatientCoordinationService.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+
+namespace LondonFhirService.Core.Services.Coordinations.Patients
+{
+    public class PatientCoordinationService : IPatientCoordinationService
+    {
+        public ValueTask<Bundle> Everything(
+            string id,
+            DateTimeOffset? start = null,
+            DateTimeOffset? end = null,
+            string typeFilter = null,
+            DateTimeOffset? since = null,
+            int? count = null,
+            CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
