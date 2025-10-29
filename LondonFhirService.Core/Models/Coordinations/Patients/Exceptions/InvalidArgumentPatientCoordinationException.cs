beb3aa30e760e523530df24c1fd5eb533da3dcdf
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-29 12:40:11 +0000

    ShouldThrowValidationExceptionOnEverythingIfIdIsNullAndLogItAsync -> FAIL

---
+++ LondonFhirService.Core/Models/Coordinations/Patients/Exceptions/InvalidArgumentPatientCoordinationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace LondonFhirService.Core.Models.Coordinations.Patients.Exceptions
+{
+    public class InvalidArgumentPatientCoordinationException : Xeption
+    {
+        public InvalidArgumentPatientCoordinationException(string message)
+            : base(message)
+        { }
+    }
+}
