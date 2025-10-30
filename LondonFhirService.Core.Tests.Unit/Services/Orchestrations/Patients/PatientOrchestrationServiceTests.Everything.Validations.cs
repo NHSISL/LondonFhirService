6fcab6091f96250b51041c233fc08484f0c77252
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-30 14:58:41 +0000

    ShouldThrowValidationExceptionOnEverythingIfNoPrimaryProvidersAndLogItAsync -> FAIL

--- LondonFhirService.Core.Tests.Unit/Services/Orchestrations/Patients/PatientOrchestrationServiceTests.Everything.Validations.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Orchestrations/Patients/PatientOrchestrationServiceTests.Everything.Validations.cs
@@ -2,9 +2,13 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Hl7.Fhir.Model;
+using LondonFhirService.Core.Models.Foundations.Providers;
 using LondonFhirService.Core.Models.Orchestrations.Patients.Exceptions;
 using Moq;
 using Task = System.Threading.Tasks.Task;
@@ -48,9 +52,66 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Orchestrations.Patients
                 .BeEquivalentTo(expectedPatientOrchestrationValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
-                    broker.LogErrorAsync(It.Is(SameExceptionAs(
-                        expectedPatientOrchestrationValidationException))),
-                Times.Once);
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedPatientOrchestrationValidationException))),
+                        Times.Once);
+
+            this.providerServiceMock.VerifyNoOtherCalls();
+            this.patientServiceMock.VerifyNoOtherCalls();
+            this.fhirReconciliationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnEverythingIfNoPrimaryProvidersAndLogItAsync()
+        {
+            // given
+            string randomId = GetRandomString();
+            string inputId = randomId;
+            CancellationToken cancellationToken = CancellationToken.None;
+
+            Provider randomActiveProvider = CreateRandomActiveProvider();
+            Provider randomInactiveProvider = CreateRandomInactiveProvider();
+
+            IQueryable<Provider> allProviders = new List<Provider>
+            {
+                randomInactiveProvider,
+                randomActiveProvider,
+            }.AsQueryable();
+
+            this.providerServiceMock.Setup(service =>
+                service.RetrieveAllProvidersAsync())
+                    .ReturnsAsync(allProviders);
+
+            var invalidPrimaryProviderPatientOrchestrationException =
+                new InvalidPrimaryProviderPatientOrchestrationException(
+                    message: "No active primary provider found. One active primary provider required.");
+
+            var expectedPatientOrchestrationValidationException =
+                new PatientOrchestrationValidationException(
+                    message: "Patient orchestration validation error occurred, please try again.",
+                    innerException: invalidPrimaryProviderPatientOrchestrationException);
+
+            // when
+            ValueTask<Bundle> everythingTask =
+                this.patientOrchestrationService.Everything(id: inputId, cancellationToken: cancellationToken);
+
+            PatientOrchestrationValidationException actualPatientOrchestrationValidationException =
+                await Assert.ThrowsAsync<PatientOrchestrationValidationException>(
+                    everythingTask.AsTask);
+
+            // then
+            actualPatientOrchestrationValidationException.Should()
+                .BeEquivalentTo(expectedPatientOrchestrationValidationException);
+
+            this.providerServiceMock.Verify(service =>
+                service.RetrieveAllProvidersAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedPatientOrchestrationValidationException))),
+                        Times.Once);
 
             this.providerServiceMock.VerifyNoOtherCalls();
             this.patientServiceMock.VerifyNoOtherCalls();
