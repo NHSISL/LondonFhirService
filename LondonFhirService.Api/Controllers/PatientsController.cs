dc2d354b5d3de69166924052477ecf5653498a7e
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-31 10:12:04 +0000

    PatientsController Everything implementation

---
+++ LondonFhirService.Api/Controllers/PatientsController.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+using LondonFhirService.Core.Services.Coordinations.Patients;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LondonFhirService.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/R4/[controller]")]
+    public class PatientsController : ControllerBase
+    {
+        private readonly IPatientCoordinationService patientCoordinationService;
+
+        public PatientsController(IPatientCoordinationService patientCoordinationService)
+        {
+            this.patientCoordinationService = patientCoordinationService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Bundle>> GetRecord(
+            string id,
+            [FromQuery] DateTimeOffset? start = null,
+            [FromQuery] DateTimeOffset? end = null,
+            [FromQuery] string _type = null,
+            [FromQuery] DateTimeOffset? _since = null,
+            [FromQuery] int? _count = null,
+            CancellationToken cancellationToken = default)
+        {
+            Bundle bundle = await this.patientCoordinationService.Everything(
+                id,
+                start,
+                end,
+                typeFilter: _type,
+                since: _since,
+                count: _count,
+                cancellationToken);
+
+            return Ok(bundle);
+        }
+    }
+}
