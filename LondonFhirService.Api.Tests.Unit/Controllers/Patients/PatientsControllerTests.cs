56f7f486a7f8ef2780c5b5d7d503d03be57d63d9
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-31 10:50:58 +0000

    ShouldReturnBadRequestOnEverythingIfValidationErrorOccurredAsync -> FAIL

--- LondonFhirService.Api.Tests.Unit/Controllers/Patients/PatientsControllerTests.cs
+++ LondonFhirService.Api.Tests.Unit/Controllers/Patients/PatientsControllerTests.cs
@@ -5,10 +5,12 @@
 using System;
 using Hl7.Fhir.Model;
 using LondonFhirService.Api.Controllers;
+using LondonFhirService.Core.Models.Coordinations.Patients.Exceptions;
 using LondonFhirService.Core.Services.Coordinations.Patients;
 using Moq;
 using RESTFulSense.Controllers;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace LondonFhirService.Api.Tests.Unit.Controllers.Patients
 {
@@ -42,5 +44,21 @@ namespace LondonFhirService.Api.Tests.Unit.Controllers.Patients
                 Total = GetRandomNumber()
             };
 
+        public static TheoryData<Xeption> ValidationExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new PatientCoordinationValidationException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new PatientCoordinationDependencyValidationException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
+        }
     }
 }
