8e0286124f0c9a5f0e2bd32acde992f77897b945
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-10-31 10:11:47 +0000

    PatientsController Everything Logic test

---
+++ LondonFhirService.Api.Tests.Unit/Controllers/Patients/PatientsControllerTests.GetRecord.Logic.cs
@@ -0,0 +1,77 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading;
+using FluentAssertions;
+using Force.DeepCloner;
+using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Task = System.Threading.Tasks.Task;
+
+namespace LondonFhirService.Api.Tests.Unit.Controllers.Patients
+{
+    public partial class PatientsControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnBundleOnGetRecordAsync()
+        {
+            // given
+            string randomId = GetRandomString();
+            string inputId = randomId;
+            DateTimeOffset? inputStart = GetRandomDateTimeOffset();
+            DateTimeOffset? inputEnd = GetRandomDateTimeOffset();
+            string inputTypeFilter = GetRandomString();
+            DateTimeOffset? inputSince = GetRandomDateTimeOffset();
+            int? inputCount = GetRandomNumber();
+            CancellationToken cancellationToken = CancellationToken.None;
+            Bundle randomBundle = CreateRandomBundle();
+            Bundle expectedBundle = randomBundle.DeepClone();
+            var expectedObjectResult = new OkObjectResult(expectedBundle);
+            var expectedActionResult = new ActionResult<Bundle>(expectedObjectResult);
+
+            this.patientCoordinationServiceMock.Setup(coordination =>
+                coordination.Everything(
+                    inputId,
+                    inputStart,
+                    inputEnd,
+                    inputTypeFilter,
+                    inputSince,
+                    inputCount,
+                    cancellationToken))
+                    .ReturnsAsync(expectedBundle);
+
+            // when
+            ActionResult<Bundle> actualActionResult =
+                await this.patientsController.GetRecord(
+                    inputId,
+                    inputStart,
+                    inputEnd,
+                    inputTypeFilter,
+                    inputSince,
+                    inputCount,
+                    cancellationToken);
+
+            // then
+            actualActionResult.Result.Should().BeOfType<OkObjectResult>();
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
+            var okResult = actualActionResult.Result as OkObjectResult;
+            okResult.Value.Should().BeEquivalentTo(expectedBundle);
+
+            this.patientCoordinationServiceMock.Verify(coordination =>
+                coordination.Everything(
+                    inputId,
+                    inputStart,
+                    inputEnd,
+                    inputTypeFilter,
+                    inputSince,
+                    inputCount,
+                    cancellationToken),
+                    Times.Once);
+
+            this.patientCoordinationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LondonFhirService.Api.Tests.Unit/Controllers/Patients/PatientsControllerTests.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using Hl7.Fhir.Model;
+using LondonFhirService.Api.Controllers;
+using LondonFhirService.Core.Services.Coordinations.Patients;
+using Moq;
+using RESTFulSense.Controllers;
+using Tynamix.ObjectFiller;
+
+namespace LondonFhirService.Api.Tests.Unit.Controllers.Patients
+{
+    public partial class PatientsControllerTests : RESTFulController
+    {
+        private readonly Mock<IPatientCoordinationService> patientCoordinationServiceMock;
+        private readonly PatientsController patientsController;
+
+        public PatientsControllerTests()
+        {
+            this.patientCoordinationServiceMock = new Mock<IPatientCoordinationService>();
+
+            this.patientsController = new PatientsController(
+                this.patientCoordinationServiceMock.Object);
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static Bundle CreateRandomBundle() =>
+            new Bundle
+            {
+                Id = GetRandomString(),
+                Type = Bundle.BundleType.Searchset,
+                Total = GetRandomNumber()
+            };
+
+    }
+}
