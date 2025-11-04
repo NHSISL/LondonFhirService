17a1fdbcc0e9a23a6bae5ef7ea3b714219bbac7e
Joe Owens <joe.owens@answerdigital.com>
2025-11-04 13:38:36 +0000

    CONTROLLERS: Stu3PatientController

---
+++ LondonFhirService.Api.Tests.Unit/Controllers/Patients/STU3/Stu3PatientsControllerTests.Everything.Security.cs
@@ -0,0 +1,81 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Attrify.Attributes;
+using FluentAssertions;
+using LondonFhirService.Api.Controllers;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LondonFhirService.Api.Tests.Unit.Controllers.Patients.STU3
+{
+    public partial class Stu3PatientControllerTests
+    {
+        [Fact]
+        public void GetShouldHaveRolesAttribute()
+        {
+            // given
+            var controllerType = typeof(Stu3PatientController);
+            var methodInfo = controllerType.GetMethod("Everything");
+            Type attributeType = typeof(AuthorizeAttribute);
+            string attributeProperty = "Roles";
+
+            List<string> expectedAttributeValues = new List<string>
+            {
+                "Patient.Everything"
+            };
+
+            // when
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // then
+            attribute.Should().NotBeNull();
+
+            var actualAttributeValue = attributeType
+                .GetProperty(attributeProperty)?
+                .GetValue(attribute) as string ?? string.Empty;
+
+            var actualAttributeValues = actualAttributeValue?
+                .Split(',')
+                .Select(role => role.Trim())
+                .Where(role => !string.IsNullOrEmpty(role))
+                .ToList();
+
+            actualAttributeValues.Should().BeEquivalentTo(expectedAttributeValues);
+        }
+
+        [Fact]
+        public void GetShouldNotHaveInvisibleApiAttribute()
+        {
+            // Given
+            var controllerType = typeof(Stu3PatientController);
+            var methodInfo = controllerType.GetMethod("Everything");
+            Type attributeType = typeof(InvisibleApiAttribute);
+
+            // When
+            var methodAttribute = methodInfo?
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var controllerAttribute = controllerType
+                .GetCustomAttributes(attributeType, inherit: true)
+                .FirstOrDefault();
+
+            var attribute = methodAttribute ?? controllerAttribute;
+
+            // Then
+            attribute.Should().BeNull();
+        }
+    }
+}
---
+++ LondonFhirService.Api/Controllers/Stu3PatientController.cs
@@ -0,0 +1,106 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+using LondonFhirService.Core.Models.Coordinations.Patients.Exceptions;
+using LondonFhirService.Core.Services.Coordinations.Patients.STU3;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace LondonFhirService.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/STU3/[controller]")]
+    public class Stu3PatientController : RESTFulController
+    {
+        private readonly IStu3PatientCoordinationService patientCoordinationService;
+
+        public Stu3PatientController(IStu3PatientCoordinationService patientCoordinationService)
+        {
+            this.patientCoordinationService = patientCoordinationService;
+        }
+
+        [HttpPost("{id}/$everything")]
+        [Authorize(Roles = "Patient.Everything")]
+        public async Task<ActionResult<Bundle>> Everything(
+            string id,
+            [FromBody] Parameters parameters,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                DateTimeOffset? start = ExtractDateTimeParameter(parameters, "start");
+                DateTimeOffset? end = ExtractDateTimeParameter(parameters, "end");
+                string typeFilter = ExtractStringParameter(parameters, "_type");
+                DateTimeOffset? since = ExtractDateTimeParameter(parameters, "_since");
+                int? count = ExtractIntParameter(parameters, "_count");
+
+                Bundle bundle = await this.patientCoordinationService.Everything(
+                    id,
+                    start,
+                    end,
+                    typeFilter,
+                    since,
+                    count,
+                    cancellationToken);
+
+                return Ok(bundle);
+            }
+            catch (PatientCoordinationValidationException patientCoordinationValidationException)
+            {
+                return BadRequest(patientCoordinationValidationException.InnerException);
+            }
+            catch (PatientCoordinationDependencyValidationException
+                   patientCoordinationDependencyValidationException)
+            {
+                return BadRequest(patientCoordinationDependencyValidationException.InnerException);
+            }
+            catch (PatientCoordinationDependencyException patientCoordinationDependencyException)
+            {
+                return InternalServerError(patientCoordinationDependencyException);
+            }
+            catch (PatientCoordinationServiceException patientCoordinationServiceException)
+            {
+                return InternalServerError(patientCoordinationServiceException);
+            }
+        }
+
+        private static string ExtractStringParameter(Parameters parameters, string name)
+        {
+            var parameter = parameters?.Parameter?.FirstOrDefault(p => p.Name == name);
+
+            return parameter?.Value is FhirString fhirString ? fhirString.Value : null;
+        }
+
+        private static DateTimeOffset? ExtractDateTimeParameter(Parameters parameters, string name)
+        {
+            var parameter = parameters?.Parameter?.FirstOrDefault(p => p.Name == name);
+
+            if (parameter?.Value is FhirDateTime fhirDateTime && fhirDateTime.Value != null)
+            {
+                return DateTimeOffset.Parse(fhirDateTime.Value);
+            }
+
+            if (parameter?.Value is Date date && date.Value != null)
+            {
+                return DateTimeOffset.Parse(date.Value);
+            }
+
+            return null;
+        }
+
+        private static int? ExtractIntParameter(Parameters parameters, string name)
+        {
+            var parameter = parameters?.Parameter?.FirstOrDefault(p => p.Name == name);
+
+            return parameter?.Value is Integer integer ? integer.Value : null;
+        }
+    }
+}
