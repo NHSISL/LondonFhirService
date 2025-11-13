846131f48ec8b6431bd935024fd3f8e0422a9f47
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-11-13 12:09:25 +0000

    ShouldThrowValidationExceptionOnGetStructuredRecordIfNoPrimaryProvidersAndLogItAsync -> FAIL

--- LondonFhirService.Core.Tests.Unit/Services/Orchestrations/Patients/R4/R4PatientOrchestrationServiceTests.GetStructuredRecord.Validations.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Orchestrations/Patients/R4/R4PatientOrchestrationServiceTests.GetStructuredRecord.Validations.cs
@@ -2,9 +2,13 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Hl7.Fhir.Model;
+using LondonFhirService.Core.Models.Foundations.Providers;
 using LondonFhirService.Core.Models.Orchestrations.Patients.Exceptions;
 using Moq;
 
@@ -46,6 +50,63 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Orchestrations.Patients.R4
             // then
             actualPatientOrchestrationValidationException.Should()
                 .BeEquivalentTo(expectedPatientOrchestrationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedPatientOrchestrationValidationException))),
+                        Times.Once);
+
+            this.providerServiceMock.VerifyNoOtherCalls();
+            this.patientServiceMock.VerifyNoOtherCalls();
+            this.fhirReconciliationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnGetStructuredRecordIfNoPrimaryProvidersAndLogItAsync()
+        {
+            // given
+            string randomId = GetRandomString();
+            string inputNhsNumber = randomId;
+            CancellationToken cancellationToken = CancellationToken.None;
+
+            Provider randomActiveProvider = CreateRandomActiveProvider();
+            Provider randomInactiveProvider = CreateRandomInactiveProvider();
+
+            IQueryable<Provider> allProviders = new List<Provider>
+            {
+                randomInactiveProvider,
+                randomActiveProvider,
+            }.AsQueryable();
+
+            this.providerServiceMock.Setup(service =>
+                service.RetrieveAllProvidersAsync())
+                    .ReturnsAsync(allProviders);
+
+            var invalidPrimaryProviderPatientOrchestrationException =
+                new InvalidPrimaryProviderPatientOrchestrationException(
+                    message: "No active primary provider found. One active primary provider required.");
+
+            var expectedPatientOrchestrationValidationException =
+                new PatientOrchestrationValidationException(
+                    message: "Patient orchestration validation error occurred, please try again.",
+                    innerException: invalidPrimaryProviderPatientOrchestrationException);
+
+            // when
+            ValueTask<Bundle> everythingTask = this.patientOrchestrationService
+                .GetStructuredRecord(nhsNumber: inputNhsNumber, cancellationToken: cancellationToken);
+
+            PatientOrchestrationValidationException actualPatientOrchestrationValidationException =
+                await Assert.ThrowsAsync<PatientOrchestrationValidationException>(
+                    everythingTask.AsTask);
+
+            // then
+            actualPatientOrchestrationValidationException.Should()
+                .BeEquivalentTo(expectedPatientOrchestrationValidationException);
+
+            this.providerServiceMock.Verify(service =>
+                service.RetrieveAllProvidersAsync(),
+                    Times.Once);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
