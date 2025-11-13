1229306640ee220cf1428c2dda5b9ee1e5d6ce07
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-11-13 11:15:42 +0000

    ShouldThrowValidationExceptionOnGetStructuredRecordIfIdIsNullAndLogItAsync -> FAIL

---
+++ LondonFhirService.Core.Tests.Unit/Services/Orchestrations/Patients/R4/R4PatientOrchestrationServiceTests.GetStructuredRecord.Validations.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Hl7.Fhir.Model;
+using LondonFhirService.Core.Models.Orchestrations.Patients.Exceptions;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Orchestrations.Patients.R4
+{
+    public partial class R4PatientOrchestrationServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnGetStructuredRecordIfIdIsNullAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            string invalidNhsNumber = invalidText;
+
+            var invalidArgumentPatientOrchestrationException =
+                new InvalidArgumentPatientOrchestrationException(
+                    message: "Invalid patient orchestration argument, please correct the errors and try again.");
+
+            invalidArgumentPatientOrchestrationException.AddData(
+                key: "NhsNumber",
+                values: "Text is required");
+
+            var expectedPatientOrchestrationValidationException =
+                new PatientOrchestrationValidationException(
+                    message: "Patient orchestration validation error occurred, please try again.",
+                    innerException: invalidArgumentPatientOrchestrationException);
+
+            // when
+            ValueTask<Bundle> everythingTask =
+                this.patientOrchestrationService.GetStructuredRecord(nhsNumber: invalidNhsNumber);
+
+            PatientOrchestrationValidationException actualPatientOrchestrationValidationException =
+                await Assert.ThrowsAsync<PatientOrchestrationValidationException>(
+                    everythingTask.AsTask);
+
+            // then
+            actualPatientOrchestrationValidationException.Should()
+                .BeEquivalentTo(expectedPatientOrchestrationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedPatientOrchestrationValidationException))),
+                        Times.Once);
+
+            this.providerServiceMock.VerifyNoOtherCalls();
+            this.patientServiceMock.VerifyNoOtherCalls();
+            this.fhirReconciliationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
