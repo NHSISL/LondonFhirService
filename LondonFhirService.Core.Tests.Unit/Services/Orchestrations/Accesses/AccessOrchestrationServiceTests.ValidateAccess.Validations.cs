af7eddbe73e936e69bf64fb0947dcd0bea7860fb
Christo du Toit <christo@dutoit.co.uk>
2026-01-16 15:08:04 +0000

    ShouldThrowForbiddenExceptionOnValidateAccessWhenOrganisationsNoAccessToPatient -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.ValidateAccess.Validations.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.ValidateAccess.Validations.cs
@@ -174,7 +174,8 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Orchestrations.Accesses
 
             var forbiddenAccessOrchestrationException =
                 new ForbiddenAccessOrchestrationException(
-                   "Current consumer is not active or does not have a valid access window.");
+                   "Current consumer is not active or does not have a valid access window.  " +
+                        $"CorrelationId: {correlationId.ToString()}");
 
             var expectedAccessOrchestrationValidationException =
                 new AccessOrchestrationValidationException(
@@ -224,7 +225,8 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Orchestrations.Accesses
 
                     $"Access was forbidden as consumer with id {inputConsumer.Id} " +
                         $"is not active / does not have valid access window " +
-                            $"(ActiveFrom: {inputConsumer.ActiveFrom}, ActiveTo: {inputConsumer.ActiveTo})",
+                            $"(ActiveFrom: {inputConsumer.ActiveFrom}, ActiveTo: {inputConsumer.ActiveTo})  " +
+                                $"CorrelationId: {correlationId.ToString()}",
                     null,
                     correlationId.ToString()),
                         Times.Once);
@@ -357,7 +359,8 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Orchestrations.Accesses
                         $"'{inputNhsNumber.Substring(0, 5)}..." +
                         $"{inputNhsNumber.Substring(inputNhsNumber.Length - 5)}' and " +
                         $"pepper '{accessConfigurations.HashPepper.Substring(0, 5)}..." +
-                        $"{accessConfigurations.HashPepper.Substring(accessConfigurations.HashPepper.Length - 5)}'",
+                        $"{accessConfigurations.HashPepper.Substring(accessConfigurations.HashPepper.Length - 5)}'  " +
+                        $"CorrelationId: {correlationId.ToString()}",
                     null,
                     correlationId.ToString()),
                         Times.Once);
