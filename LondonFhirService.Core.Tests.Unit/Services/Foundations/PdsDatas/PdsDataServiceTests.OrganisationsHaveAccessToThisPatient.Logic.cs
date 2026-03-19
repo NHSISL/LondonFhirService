a548360ee5df166f015ef7b88027a4648349fdf3
Christo du Toit <christo@dutoit.co.uk>
2026-03-19 15:20:37 +0000

    ShouldNotHaveAccessOnCheckIfOrganisationsHaveAccessToThisPatientWithMissingConfigAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/PdsDatas/PdsDataServiceTests.OrganisationsHaveAccessToThisPatient.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/PdsDatas/PdsDataServiceTests.OrganisationsHaveAccessToThisPatient.Logic.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using LondonFhirService.Core.Models.Foundations.PdsDatas;
+using LondonFhirService.Core.Models.Foundations.PdsDatas.Exceptions;
 using Moq;
 
 namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.PdsDatas
@@ -130,6 +131,61 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.PdsDatas
             this.dateTimeBroker.Verify(broker =>
                 broker.GetCurrentDateTimeOffsetAsync(),
                     Times.Once);
+
+            this.storageBroker.VerifyNoOtherCalls();
+            this.dateTimeBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldNotHaveAccessOnCheckIfOrganisationsHaveAccessToThisPatientWithMissingConfigAsync()
+        {
+            // given
+            string randomPseudoNhsNumber = GetRandomString();
+            string inputPseudoNhsNumber = randomPseudoNhsNumber;
+            List<string> inputOrganisationCodes = GetRandomStringsWithLengthOf(10);
+
+            this.storageBroker.Setup(broker =>
+                broker.SelectAllPdsDatasAsync())
+                    .ReturnsAsync(new List<PdsData>().AsQueryable());
+
+            this.dateTimeBroker.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(DateTimeOffset.UtcNow);
+
+            var invalidPdsDataException =
+                new InvalidPdsDataServiceException(
+                    message: "PDS configuration data not found for patient.");
+
+            PdsDataServiceValidationException expectedPdsDataServiceValidationException =
+                new PdsDataServiceValidationException(
+                    message: "PdsData validation error occurred, please fix errors and try again.",
+                    innerException: invalidPdsDataException);
+
+            // when
+            ValueTask<bool> retrieveAllPdsDatasTask =
+                this.pdsDataService.OrganisationsHaveAccessToThisPatient(
+                    nhsNumber: inputPseudoNhsNumber, organisationCodes: inputOrganisationCodes);
+
+            PdsDataServiceValidationException actualPdsDataServiceValidationException =
+                await Assert.ThrowsAsync<PdsDataServiceValidationException>(
+                    testCode: retrieveAllPdsDatasTask.AsTask);
+
+            // then
+            actualPdsDataServiceValidationException.Should().BeEquivalentTo(expectedPdsDataServiceValidationException);
+
+            this.storageBroker.Verify(broker =>
+                broker.SelectAllPdsDatasAsync(),
+                    Times.Once);
+
+            this.dateTimeBroker.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedPdsDataServiceValidationException))),
+                        Times.Once);
 
             this.storageBroker.VerifyNoOtherCalls();
             this.dateTimeBroker.VerifyNoOtherCalls();
