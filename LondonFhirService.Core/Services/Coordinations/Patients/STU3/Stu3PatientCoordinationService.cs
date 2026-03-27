b17277b8efbfa2ee4ff06221bccb22969a59371e
Christo du Toit <christo@dutoit.co.uk>
2026-03-27 14:57:28 +0000

    ShouldCallGetStructuredRecordAsync -> PASS

--- LondonFhirService.Core/Services/Coordinations/Patients/STU3/Stu3PatientCoordinationService.cs
+++ LondonFhirService.Core/Services/Coordinations/Patients/STU3/Stu3PatientCoordinationService.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using LondonFhirService.Core.Brokers.Audits;
 using LondonFhirService.Core.Brokers.Identifiers;
 using LondonFhirService.Core.Brokers.Loggings;
+using LondonFhirService.Core.Models.Orchestrations.Accesses;
 using LondonFhirService.Core.Services.Orchestrations.Accesses;
 using LondonFhirService.Core.Services.Orchestrations.Patients.STU3;
 
@@ -21,19 +22,22 @@ namespace LondonFhirService.Core.Services.Coordinations.Patients.STU3
         private readonly ILoggingBroker loggingBroker;
         private readonly IAuditBroker auditBroker;
         private readonly IIdentifierBroker identifierBroker;
+        private readonly AccessConfigurations accessConfigurations;
 
         public Stu3PatientCoordinationService(
             IAccessOrchestrationService accessOrchestrationService,
             IStu3PatientOrchestrationService patientOrchestrationService,
             ILoggingBroker loggingBroker,
             IAuditBroker auditBroker,
-            IIdentifierBroker identifierBroker)
+            IIdentifierBroker identifierBroker,
+            AccessConfigurations accessConfigurations)
         {
             this.accessOrchestrationService = accessOrchestrationService;
             this.patientOrchestrationService = patientOrchestrationService;
             this.loggingBroker = loggingBroker;
             this.auditBroker = auditBroker;
             this.identifierBroker = identifierBroker;
+            this.accessConfigurations = accessConfigurations;
         }
 
         public ValueTask<string> GetStructuredRecordSerialisedAsync(
@@ -61,14 +65,17 @@ namespace LondonFhirService.Core.Services.Coordinations.Patients.STU3
                 fileName: null,
                 correlationId: correlationId.ToString());
 
-            await this.auditBroker.LogInformationAsync(
-                auditType,
-                title: $"Check Access Permissions",
-                message,
-                fileName: null,
-                correlationId: correlationId.ToString());
+            if (accessConfigurations.CheckAccessPermissions == true)
+            {
+                await this.auditBroker.LogInformationAsync(
+                    auditType,
+                    title: $"Check Access Permissions",
+                    message,
+                    fileName: null,
+                    correlationId: correlationId.ToString());
 
-            await this.accessOrchestrationService.ValidateAccess(nhsNumber, correlationId);
+                await this.accessOrchestrationService.ValidateAccess(nhsNumber, correlationId);
+            }
 
             await this.auditBroker.LogInformationAsync(
                 auditType,
