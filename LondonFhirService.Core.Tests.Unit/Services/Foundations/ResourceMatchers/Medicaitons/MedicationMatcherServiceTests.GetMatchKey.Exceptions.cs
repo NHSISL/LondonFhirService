db715d8292513d3aae5b788ce8e18ef4fb6570d8
hassaero110 <h_raza110@outlook.com>
2026-03-31 08:25:53 +0100

    FOUNDATIONS: Medication Matcher Service

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Medicaitons/MedicationMatcherServiceTests.GetMatchKey.Exceptions.cs
@@ -0,0 +1,80 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers.Medications.Exceptions;
+using LondonFhirService.Core.Services.Foundations.ResourceMatchers.Medications;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.Medications
+{
+    public partial class MedicationMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnGetMatchKeysIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            JsonElement resource = new();
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+            var serviceException = new Exception();
+
+            var failedMedicationMatcherServiceException =
+                new FailedMedicationMatcherServiceException(
+                    message: "Failed medication matcher service occurred, please contact support",
+                    innerException: serviceException);
+
+            var expectedMedicationMatcherServiceException =
+                new MedicationMatcherServiceException(
+                    message: "Medication matcher service error occurred, contact support.",
+                    innerException: failedMedicationMatcherServiceException);
+
+            var medicationMatcherServiceMock = new Mock<MedicationMatcherService>(loggingBrokerMock.Object)
+                { CallBase = true };
+
+            medicationMatcherServiceMock.Setup(service =>
+                service.ValidateOnGetMatchKeyArguments(
+                    resource,
+                    resourceIndex))
+                        .Throws(serviceException);
+
+            // when
+            ValueTask<string> matchTask =
+                medicationMatcherServiceMock.Object.GetMatchKeyAsync(
+                    resource,
+                    resourceIndex);
+
+            MedicationMatcherServiceException actualMedicationMatcherServiceException =
+                await Assert.ThrowsAsync<MedicationMatcherServiceException>(
+                    matchTask.AsTask);
+
+            // then
+            actualMedicationMatcherServiceException.Should()
+                .BeEquivalentTo(expectedMedicationMatcherServiceException);
+
+            medicationMatcherServiceMock.Verify(service =>
+                service.ValidateOnGetMatchKeyArguments(
+                    resource,
+                    resourceIndex),
+                        Times.Once);
+
+            medicationMatcherServiceMock.Verify(service =>
+                service.GetMatchKeyAsync(
+                    resource,
+                    resourceIndex),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedMedicationMatcherServiceException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            medicationMatcherServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}    
