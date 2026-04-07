1da34e8a758ff04080e7be4262db2f4e6de74391
hassaero110 <h_raza110@outlook.com>
2026-04-07 09:06:08 +0100

    PROCESSINGS: List Entry Comparison Validaitons and Exceptions Tests

---
+++ LondonFhirService.Core.Tests.Unit/Services/Processings/ListEntryComparisons/ListEntryComparisonProcessingServiceTests.CompareListEntryCounts.Validations.cs
@@ -0,0 +1,68 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Processings.JsonIgnoreRules.ArrayOrderIgnoreRules.Exceptions;
+using LondonFhirService.Core.Models.Processings.JsonIgnoreRules.Exceptions;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Processings.ListEntryComparisons
+{
+    public partial class ArrayOrderIgnoreProcessingRuleTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnShouldIgnoreIfResourceIsInvalidAsync(string invalidText)
+        {
+            // given
+            JsonElement invalidElement = default;
+            string invalidPath = invalidText;
+
+            var invalidJsonIgnoreProcessingException =
+                new InvalidJsonIgnoreProcessingException(
+                    message: "Invalid arguments. Please correct the errors and try again.");
+
+            invalidJsonIgnoreProcessingException.AddData(
+                key: "element",
+                values: "Json element is undefined.");
+
+            invalidJsonIgnoreProcessingException.UpsertDataList(
+                key: "path",
+                value: "Text is invalid.");
+
+            var expectedArrayOrderIgnoreProcessingValidationException =
+                new ArrayOrderIgnoreProcessingValidationException(
+                    message:
+                        "Array order ignore processing validation error occurred, " +
+                        "please fix errors and try again.",
+                    innerException: invalidJsonIgnoreProcessingException);
+
+            // when
+            ValueTask<bool> shouldIgnoreTask =
+                this.arrayOrderIgnoreProcessingRule.ShouldIgnoreAsync(
+                    invalidElement,
+                    invalidPath);
+
+            // then
+            ArrayOrderIgnoreProcessingValidationException actualException =
+                await Assert.ThrowsAsync<ArrayOrderIgnoreProcessingValidationException>(
+                    shouldIgnoreTask.AsTask);
+
+            actualException.Should()
+                .BeEquivalentTo(expectedArrayOrderIgnoreProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedArrayOrderIgnoreProcessingValidationException))),
+                       Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.jsonElementServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
