9544323362bd3c58cfb5b7ba3a99c0bf579a1863
Raghu Peri <perirrs@gmail.com>
2026-04-29 19:39:23 +0000

    ShouldGetMatchKeyWhenPractitionerRoleHasSdsRoleProfileIdAsync -> PASS

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/PractitionerRoles/PractitionerRolesMatcherServiceTests.GetMatchKey.Logic.cs
@@ -0,0 +1,37 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.PractitionerRoles
+{
+    public partial class PractitionerRoleMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldGetMatchKeyWhenPractitionerRoleHasSdsRoleProfileIdAsync()
+        {
+            // given
+            string inputSdsRoleProfileId = GetRandomSdsRoleProfileIdValue();
+            string randomId = GetRandomString();
+
+            JsonElement practitionerRoleResource = CreatePractitionerRoleResource(
+                sdsRoleProfileId: inputSdsRoleProfileId,
+                id: randomId);
+
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+            string expectedMatchKey = inputSdsRoleProfileId;
+
+            // when
+            string actualMatchKey =
+                await this.practitionerRoleMatcherService.GetMatchKeyAsync(practitionerRoleResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().Be(expectedMatchKey);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
