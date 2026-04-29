700e3a7d06303ed8ef47c62ca3d9f382e9cecd64
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:03:34 +0000

    ShouldAddToUnmatchedFromSource2WhenOnlySource2HasFamilyMemberHistoryAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/FamilyMemberHistories/FamilyMemberHistoriesMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/FamilyMemberHistories/FamilyMemberHistoriesMatcherServiceTests.Match.Logic.cs
@@ -79,5 +79,37 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldAddToUnmatchedFromSource2WhenOnlySource2HasFamilyMemberHistoryAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = "FMH-1";
+
+            JsonElement source2Resource = CreateFamilyMemberHistoryResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "family-member-history-1");
+
+            var source1Resources = new List<JsonElement>();
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Unmatched.Add(
+                new UnmatchedResource(source2Resource, "FamilyMemberHistory", inputDdsIdentifierValue, false));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.familyMemberHistoryMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
