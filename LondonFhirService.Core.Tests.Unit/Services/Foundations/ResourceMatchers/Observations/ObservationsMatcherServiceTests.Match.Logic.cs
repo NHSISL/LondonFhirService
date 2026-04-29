e3a930e807eb157d991cae9809024fe1f1d8b852
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:35:30 +0000

    ShouldMatchObservationsWhenBothSourcesHaveResourcesWithSameKeyAsync -> FAIL

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Observations/ObservationsMatcherServiceTests.Match.Logic.cs
@@ -0,0 +1,51 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.Observations
+{
+    public partial class ObservationMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldMatchObservationsWhenBothSourcesHaveResourcesWithSameKeyAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = "OBS-1";
+
+            JsonElement source1Resource = CreateObservationResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "observation-1");
+
+            JsonElement source2Resource = CreateObservationResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "observation-2");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Matched.Add(
+                new MatchedResource(source1Resource, source2Resource, inputDdsIdentifierValue));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.observationMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
