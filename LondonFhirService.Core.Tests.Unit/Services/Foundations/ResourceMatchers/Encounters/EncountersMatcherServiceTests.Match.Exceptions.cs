6713125185128275940dcb3efc96ff499e83259e
Raghu Peri <perirrs@gmail.com>
2026-04-29 15:22:42 +0000

    ShouldThrowServiceExceptionOnMatchIfServiceErrorOccursAndLogItAsync -> PASS

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Encounters/EncountersMatcherServiceTests.Match.Exceptions.cs
@@ -0,0 +1,91 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers.Exceptions;
+using LondonFhirService.Core.Services.Foundations.ResourceMatchers.Encounters;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.Encounters
+{
+    public partial class EncounterMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnMatchIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            List<JsonElement> invalidSource1Resources = new();
+            List<JsonElement> invalidSource2Resources = new();
+            Dictionary<string, JsonElement> invalidSource1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> invalidSource2ResourceIndex = CreateResourceIndex();
+            var serviceException = new Exception();
+
+            var failedResourceMatcherServiceException =
+                new FailedResourceMatcherServiceException(
+                    message: "Failed encounter matcher service occurred, please contact support",
+                    innerException: serviceException);
+
+            var expectedResourceMatcherServiceException =
+                new ResourceMatcherServiceException(
+                    message: "Encounter matcher service error occurred, contact support.",
+                    innerException: failedResourceMatcherServiceException);
+
+            var encounterMatcherServiceMock = new Mock<EncounterMatcherService>(loggingBrokerMock.Object)
+                { CallBase = true };
+
+            encounterMatcherServiceMock.Setup(service =>
+                service.ValidateOnMatchArguments(
+                    invalidSource1Resources,
+                    invalidSource2Resources,
+                    invalidSource1ResourceIndex,
+                    invalidSource2ResourceIndex))
+                        .Throws(serviceException);
+
+            // when
+            ValueTask<ResourceMatch> matchTask =
+                encounterMatcherServiceMock.Object.MatchAsync(
+                    invalidSource1Resources,
+                    invalidSource2Resources,
+                    invalidSource1ResourceIndex,
+                    invalidSource2ResourceIndex);
+
+            ResourceMatcherServiceException actualResourceMatcherServiceException =
+                await Assert.ThrowsAsync<ResourceMatcherServiceException>(
+                    matchTask.AsTask);
+
+            // then
+            actualResourceMatcherServiceException.Should()
+                .BeEquivalentTo(expectedResourceMatcherServiceException);
+
+            encounterMatcherServiceMock.Verify(service =>
+                service.ValidateOnMatchArguments(
+                    invalidSource1Resources,
+                    invalidSource2Resources,
+                    invalidSource1ResourceIndex,
+                    invalidSource2ResourceIndex),
+                        Times.Once);
+
+            encounterMatcherServiceMock.Verify(service =>
+                service.MatchAsync(
+                    invalidSource1Resources,
+                    invalidSource2Resources,
+                    invalidSource1ResourceIndex,
+                    invalidSource2ResourceIndex),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedResourceMatcherServiceException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            encounterMatcherServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
