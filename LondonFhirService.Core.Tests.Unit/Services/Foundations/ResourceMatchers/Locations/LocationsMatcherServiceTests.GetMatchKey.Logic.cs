d817c12f7f8bf202e7ddd0be90cc8beb523ce2cf
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:25:02 +0000

    ShouldReturnNullOnGetMatchKeyWhenLocationHasNoIdentifierPropertyAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Locations/LocationsMatcherServiceTests.GetMatchKey.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Locations/LocationsMatcherServiceTests.GetMatchKey.Logic.cs
@@ -33,5 +33,25 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualMatchKey.Should().Be(expectedMatchKey);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldReturnNullOnGetMatchKeyWhenLocationHasNoIdentifierPropertyAsync()
+        {
+            // given
+            string randomId = GetRandomString();
+
+            JsonElement locationResource =
+                CreateLocationResourceWithoutIdentifierProperty(id: randomId);
+
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+
+            // when
+            string actualMatchKey =
+                await this.locationMatcherService.GetMatchKeyAsync(locationResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().BeNull();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
