b5183f44cd2e903568e3e3bc72c2d9bb8ac8f54b
Raghu Peri <perirrs@gmail.com>
2026-04-29 20:08:22 +0000

    ShouldAddToUnmatchedFromSource1WhenOnlySource1HasReferralRequestAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ReferralRequests/ReferralRequestsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ReferralRequests/ReferralRequestsMatcherServiceTests.Match.Logic.cs
@@ -47,5 +47,37 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldAddToUnmatchedFromSource1WhenOnlySource1HasReferralRequestAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = "RR-1";
+
+            JsonElement source1Resource = CreateReferralRequestResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "referral-request-1");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement>();
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Unmatched.Add(
+                new UnmatchedResource(source1Resource, "ReferralRequest", inputDdsIdentifierValue, true));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.referralRequestMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
