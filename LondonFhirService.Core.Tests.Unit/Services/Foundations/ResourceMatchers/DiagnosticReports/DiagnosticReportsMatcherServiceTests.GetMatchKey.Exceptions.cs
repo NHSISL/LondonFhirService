81f48faf90d1f4f2cbac1457279af2d20ea00e20
Raghu Peri <perirrs@gmail.com>
2026-04-29 15:15:35 +0000

    ShouldThrowServiceExceptionOnGetMatchKeyIfServiceErrorOccursAndLogItAsync -> PASS

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/DiagnosticReports/DiagnosticReportsMatcherServiceTests.GetMatchKey.Exceptions.cs
@@ -0,0 +1,80 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers.Exceptions;
+using LondonFhirService.Core.Services.Foundations.ResourceMatchers.DiagnosticReports;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.DiagnosticReports
+{
+    public partial class DiagnosticReportMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnGetMatchKeyIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            JsonElement resource = new();
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+            var serviceException = new Exception();
+
+            var failedResourceMatcherServiceException =
+                new FailedResourceMatcherServiceException(
+                    message: "Failed diagnostic report matcher service occurred, please contact support",
+                    innerException: serviceException);
+
+            var expectedResourceMatcherServiceException =
+                new ResourceMatcherServiceException(
+                    message: "Diagnostic report matcher service error occurred, contact support.",
+                    innerException: failedResourceMatcherServiceException);
+
+            var diagnosticReportMatcherServiceMock = new Mock<DiagnosticReportMatcherService>(loggingBrokerMock.Object)
+                { CallBase = true };
+
+            diagnosticReportMatcherServiceMock.Setup(service =>
+                service.ValidateOnGetMatchKeyArguments(
+                    resource,
+                    resourceIndex))
+                        .Throws(serviceException);
+
+            // when
+            ValueTask<string> matchTask =
+                diagnosticReportMatcherServiceMock.Object.GetMatchKeyAsync(
+                    resource,
+                    resourceIndex);
+
+            ResourceMatcherServiceException actualResourceMatcherServiceException =
+                await Assert.ThrowsAsync<ResourceMatcherServiceException>(
+                    matchTask.AsTask);
+
+            // then
+            actualResourceMatcherServiceException.Should()
+                .BeEquivalentTo(expectedResourceMatcherServiceException);
+
+            diagnosticReportMatcherServiceMock.Verify(service =>
+                service.ValidateOnGetMatchKeyArguments(
+                    resource,
+                    resourceIndex),
+                        Times.Once);
+
+            diagnosticReportMatcherServiceMock.Verify(service =>
+                service.GetMatchKeyAsync(
+                    resource,
+                    resourceIndex),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedResourceMatcherServiceException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            diagnosticReportMatcherServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
