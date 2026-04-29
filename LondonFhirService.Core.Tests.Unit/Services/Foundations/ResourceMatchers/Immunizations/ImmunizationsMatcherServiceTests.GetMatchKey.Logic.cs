21c7af35d45d9383d14bb5abb21865d425480062
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:14:11 +0000

    ShouldGetMatchKeyWhenImmunizationHasDdsIdentifierAsync -> PASS

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Immunizations/ImmunizationsMatcherServiceTests.GetMatchKey.Logic.cs
@@ -0,0 +1,37 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.Immunizations
+{
+    public partial class ImmunizationMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldGetMatchKeyWhenImmunizationHasDdsIdentifierAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = GetRandomDdsIdentifierValue();
+            string randomId = GetRandomString();
+
+            JsonElement immunizationResource = CreateImmunizationResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: randomId);
+
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+            string expectedMatchKey = inputDdsIdentifierValue;
+
+            // when
+            string actualMatchKey =
+                await this.immunizationMatcherService.GetMatchKeyAsync(immunizationResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().Be(expectedMatchKey);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
