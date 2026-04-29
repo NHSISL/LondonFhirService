cd7bfcf073cffa62d9ff0b90b0bddc5a104309b8
Raghu Peri <perirrs@gmail.com>
2026-04-29 13:20:55 +0000

    ShouldReturnDdsIdentifierAsMatchKeyForAppointmentWithDdsIdentifierAsync -> FAIL

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Appointments/AppointmentMatcherServiceTests.GetMatchKey.Logic.cs
@@ -0,0 +1,36 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.Appointments
+{
+    public partial class AppointmentMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnDdsIdentifierAsMatchKeyForAppointmentWithDdsIdentifierAsync()
+        {
+            // given
+            string expectedDdsIdentifierValue = GetRandomDdsIdentifierValue();
+
+            JsonElement appointmentResource =
+                CreateAppointmentWithDdsIdentifier(expectedDdsIdentifierValue);
+
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+
+            // when
+            string actualMatchKey =
+                await this.appointmentMatcherService.GetMatchKeyAsync(
+                    appointmentResource,
+                    resourceIndex);
+
+            // then
+            actualMatchKey.Should().Be(expectedDdsIdentifierValue);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
