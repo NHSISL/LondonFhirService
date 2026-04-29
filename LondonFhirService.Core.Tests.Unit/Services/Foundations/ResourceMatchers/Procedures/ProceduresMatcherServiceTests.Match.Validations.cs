8287287a22314addc72b63a72295fed046c3b0bf
Raghu Peri <perirrs@gmail.com>
2026-04-29 19:51:23 +0000

    ShouldThrowValidationExceptionOnMatchIfArgumentsAreInvalidAsync -> PASS

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Procedures/ProceduresMatcherServiceTests.Match.Validations.cs
@@ -0,0 +1,76 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers.Exceptions;
+using Moq;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.Procedures
+{
+    public partial class ProcedureMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnMatchIfArgumentsAreInvalidAsync()
+        {
+            // given
+            List<JsonElement> invalidSource1Resources = null;
+            List<JsonElement> invalidSource2Resources = null;
+            Dictionary<string, JsonElement> invalidSource1ResourceIndex = null;
+            Dictionary<string, JsonElement> invalidSource2ResourceIndex = null;
+
+            var invalidArgumentResourceMatcherException =
+                new InvalidArgumentResourceMatcherException(
+                    message: "Resource matcher arguments are invalid. Please correct the errors and try again.");
+
+            invalidArgumentResourceMatcherException.AddData(
+                key: "source1Resources",
+                values: "List is required.");
+
+            invalidArgumentResourceMatcherException.AddData(
+                key: "source2Resources",
+                values: "List is required.");
+
+            invalidArgumentResourceMatcherException.AddData(
+                key: "source1ResourceIndex",
+                values: "Dictionary is required.");
+
+            invalidArgumentResourceMatcherException.AddData(
+                key: "source2ResourceIndex",
+                values: "Dictionary is required.");
+
+            var expectedResourceMatcherServiceValidationException =
+                new ResourceMatcherServiceValidationException(
+                    message: "Procedure matcher validation errors occurred, " +
+                        "please try again.",
+                    innerException: invalidArgumentResourceMatcherException);
+
+            // when
+            ValueTask<ResourceMatch> matchTask =
+                this.procedureMatcherService.MatchAsync(
+                    invalidSource1Resources,
+                    invalidSource2Resources,
+                    invalidSource1ResourceIndex,
+                    invalidSource2ResourceIndex);
+
+            // then
+            ResourceMatcherServiceValidationException actualException =
+                await Assert.ThrowsAsync<ResourceMatcherServiceValidationException>(
+                    matchTask.AsTask);
+
+            actualException.Should()
+                .BeEquivalentTo(expectedResourceMatcherServiceValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedResourceMatcherServiceValidationException))),
+                        Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
