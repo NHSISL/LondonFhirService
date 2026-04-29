ab3ede1c7bda375eb081913f2c7f467ef5998113
Raghu Peri <perirrs@gmail.com>
2026-04-29 20:00:29 +0000

    ShouldGetMatchKeyWhenProcedureRequestHasDdsIdentifierAsync -> PASS

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ProcedureRequests/ProcedureRequestsMatcherServiceTests.GetMatchKey.Logic.cs
@@ -0,0 +1,37 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.ProcedureRequests
+{
+    public partial class ProcedureRequestMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldGetMatchKeyWhenProcedureRequestHasDdsIdentifierAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = GetRandomDdsIdentifierValue();
+            string randomId = GetRandomString();
+
+            JsonElement procedureRequestResource = CreateProcedureRequestResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: randomId);
+
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+            string expectedMatchKey = inputDdsIdentifierValue;
+
+            // when
+            string actualMatchKey =
+                await this.procedureRequestMatcherService.GetMatchKeyAsync(procedureRequestResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().Be(expectedMatchKey);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
