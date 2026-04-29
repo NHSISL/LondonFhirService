f2f58329edd2404120e0f60c1ab759384137f6db
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:30:26 +0000

    ShouldMatchMedicationRequestsWhenBothSourcesHaveResourcesWithSameKeyAsync -> PASS

--- LondonFhirService.Core/Services/Foundations/ResourceMatchers/MedicationRequests/MedicationRequestMatcherService.cs
+++ LondonFhirService.Core/Services/Foundations/ResourceMatchers/MedicationRequests/MedicationRequestMatcherService.cs
@@ -37,12 +37,62 @@ namespace LondonFhirService.Core.Services.Foundations.ResourceMatchers.Medicatio
         TryCatch(async () =>
         {
             ValidateOnMatchArguments(source1Resources, source2Resources, source1ResourceIndex, source2ResourceIndex);
+            var resourceMatch = new ResourceMatch();
 
-            return new ResourceMatch();
+            var source1ByKey = source1Resources
+                .Select(r => new { Resource = r, Key = InternalGetMatchKey(r, source1ResourceIndex) })
+                .Where(x => x.Key != null)
+                .ToDictionary(x => x.Key!, x => x.Resource);
+
+            var source2ByKey = source2Resources
+                .Select(r => new { Resource = r, Key = InternalGetMatchKey(r, source2ResourceIndex) })
+                .Where(x => x.Key != null)
+                .ToDictionary(x => x.Key!, x => x.Resource);
+
+            var allKeys = source1ByKey.Keys.Union(source2ByKey.Keys).ToList();
+
+            foreach (var key in allKeys)
+            {
+                var hasSource1 = source1ByKey.TryGetValue(key, out var source1Resource);
+                var hasSource2 = source2ByKey.TryGetValue(key, out var source2Resource);
+
+                if (hasSource1 && hasSource2)
+                {
+                    resourceMatch.Matched.Add(new MatchedResource(source1Resource!, source2Resource!, key));
+                }
+                else if (hasSource1)
+                {
+                    resourceMatch.Unmatched.Add(new UnmatchedResource(source1Resource!, ResourceType, key, true));
+                }
+                else if (hasSource2)
+                {
+                    resourceMatch.Unmatched.Add(new UnmatchedResource(source2Resource!, ResourceType, key, false));
+                }
+            }
+
+            return resourceMatch;
         });
 
         internal virtual string InternalGetMatchKey(JsonElement resource, Dictionary<string, JsonElement> resourceIndex)
         {
+            if (!resource.TryGetProperty("identifier", out var identifiers))
+                return null;
+
+            foreach (var identifierElement in identifiers.EnumerateArray())
+            {
+                if (!identifierElement.TryGetProperty("system", out var system))
+                    continue;
+
+                var systemValue = system.GetString();
+                if (systemValue == DdsIdentifierSystem)
+                {
+                    if (identifierElement.TryGetProperty("value", out var value))
+                    {
+                        return value.GetString();
+                    }
+                }
+            }
+
             return null;
         }
     }
