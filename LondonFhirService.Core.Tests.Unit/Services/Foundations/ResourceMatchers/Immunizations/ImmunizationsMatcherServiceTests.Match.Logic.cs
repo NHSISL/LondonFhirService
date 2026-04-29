aea1a0a682c77f79313c05d2fe3f3d5a8f25d6cc
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:13:24 +0000

    ShouldAddToUnmatchedFromSource1WhenOnlySource1HasImmunizationAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Immunizations/ImmunizationsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Immunizations/ImmunizationsMatcherServiceTests.Match.Logic.cs
@@ -47,5 +47,37 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldAddToUnmatchedFromSource1WhenOnlySource1HasImmunizationAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = "IMM-1";
+
+            JsonElement source1Resource = CreateImmunizationResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "immunization-1");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement>();
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Unmatched.Add(
+                new UnmatchedResource(source1Resource, "Immunization", inputDdsIdentifierValue, true));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.immunizationMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
