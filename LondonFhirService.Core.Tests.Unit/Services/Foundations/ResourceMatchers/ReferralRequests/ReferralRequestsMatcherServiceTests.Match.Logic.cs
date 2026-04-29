8b515f5151d334bfb319f7c7477fe060bcc98051
Raghu Peri <perirrs@gmail.com>
2026-04-29 20:08:53 +0000

    ShouldExcludeReferralRequestsWithNoDdsIdentifierFromMatchResultsAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ReferralRequests/ReferralRequestsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ReferralRequests/ReferralRequestsMatcherServiceTests.Match.Logic.cs
@@ -111,5 +111,29 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldExcludeReferralRequestsWithNoDdsIdentifierFromMatchResultsAsync()
+        {
+            // given
+            JsonElement source1Resource = CreateNonDdsReferralRequestResource(id: "referral-request-1");
+            JsonElement source2Resource = CreateNonDdsReferralRequestResource(id: "referral-request-2");
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+            var expectedResourceMatch = new ResourceMatch();
+
+            // when
+            ResourceMatch actualResourceMatch = await this.referralRequestMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
