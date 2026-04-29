4dab979a93b737d0d0dd8cbde9c88afd062721d6
Raghu Peri <perirrs@gmail.com>
2026-04-29 20:22:13 +0000

    CODE RUB: Add comprehensive JSON 4.x smoke test for List matcher

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Lists/ListMatcherServiceTests.Match.Logic.cs
@@ -0,0 +1,51 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.Lists
+{
+    public partial class ListMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldMatchComprehensiveListsWithMultipleIdentifierSystemsAsync()
+        {
+            // given
+            string title = "Active Problem List";
+
+            JsonElement source1Resource = CreateComprehensiveListResource(
+                title: title,
+                id: "list-comprehensive-1");
+
+            JsonElement source2Resource = CreateComprehensiveListResource(
+                title: title,
+                id: "list-comprehensive-2");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Matched.Add(
+                new MatchedResource(source1Resource, source2Resource, title));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.listMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
