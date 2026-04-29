102545dbc52d73e8f6565b544308db011c7e3160
Raghu Peri <perirrs@gmail.com>
2026-04-29 19:51:04 +0000

    ShouldReturnNullOnGetMatchKeyWhenProcedureHasNonDdsIdentifierAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Procedures/ProceduresMatcherServiceTests.GetMatchKey.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Procedures/ProceduresMatcherServiceTests.GetMatchKey.Logic.cs
@@ -53,5 +53,22 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualMatchKey.Should().BeNull();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldReturnNullOnGetMatchKeyWhenProcedureHasNonDdsIdentifierAsync()
+        {
+            // given
+            string randomId = GetRandomString();
+            JsonElement procedureResource = CreateNonDdsProcedureResource(id: randomId);
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+
+            // when
+            string actualMatchKey =
+                await this.procedureMatcherService.GetMatchKeyAsync(procedureResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().BeNull();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
