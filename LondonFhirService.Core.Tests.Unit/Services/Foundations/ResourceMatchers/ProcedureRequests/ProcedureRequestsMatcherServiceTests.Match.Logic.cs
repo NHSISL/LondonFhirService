69b1a6f5ec9af9bc01df08b32d08dcea437a3cf7
Raghu Peri <perirrs@gmail.com>
2026-04-29 20:02:16 +0000

    ShouldMatchComprehensiveProcedureRequestsWithMultipleIdentifierSystemsAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ProcedureRequests/ProcedureRequestsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ProcedureRequests/ProcedureRequestsMatcherServiceTests.Match.Logic.cs
@@ -113,6 +113,42 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
         }
 
         [Fact]
+        public async Task ShouldMatchComprehensiveProcedureRequestsWithMultipleIdentifierSystemsAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = "PRREQ-comprehensive-1";
+
+            JsonElement source1Resource = CreateComprehensiveProcedureRequestResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "procedure-request-comprehensive-1");
+
+            JsonElement source2Resource = CreateComprehensiveProcedureRequestResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "procedure-request-comprehensive-2");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Matched.Add(
+                new MatchedResource(source1Resource, source2Resource, inputDdsIdentifierValue));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.procedureRequestMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldExcludeProcedureRequestsWithNoDdsIdentifierFromMatchResultsAsync()
         {
             // given
