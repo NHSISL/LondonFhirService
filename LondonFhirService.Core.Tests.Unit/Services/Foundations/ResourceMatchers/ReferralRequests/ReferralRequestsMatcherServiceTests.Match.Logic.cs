c2eabda59f22a63dcec6745a308bfcebb5b460f9
Raghu Peri <perirrs@gmail.com>
2026-04-29 20:11:01 +0000

    ShouldMatchComprehensiveReferralRequestsWithMultipleIdentifierSystemsAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ReferralRequests/ReferralRequestsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ReferralRequests/ReferralRequestsMatcherServiceTests.Match.Logic.cs
@@ -113,6 +113,42 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
         }
 
         [Fact]
+        public async Task ShouldMatchComprehensiveReferralRequestsWithMultipleIdentifierSystemsAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = "RR-comprehensive-1";
+
+            JsonElement source1Resource = CreateComprehensiveReferralRequestResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "referral-request-comprehensive-1");
+
+            JsonElement source2Resource = CreateComprehensiveReferralRequestResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "referral-request-comprehensive-2");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Matched.Add(
+                new MatchedResource(source1Resource, source2Resource, inputDdsIdentifierValue));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.referralRequestMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldExcludeReferralRequestsWithNoDdsIdentifierFromMatchResultsAsync()
         {
             // given
