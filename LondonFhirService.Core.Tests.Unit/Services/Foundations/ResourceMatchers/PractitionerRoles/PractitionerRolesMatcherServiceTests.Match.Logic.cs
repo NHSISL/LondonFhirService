29b8fa1c53158521ddf14d7b962445afbd87e80f
Raghu Peri <perirrs@gmail.com>
2026-04-29 19:41:06 +0000

    ShouldMatchComprehensivePractitionerRolesWithMultipleIdentifierSystemsAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/PractitionerRoles/PractitionerRolesMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/PractitionerRoles/PractitionerRolesMatcherServiceTests.Match.Logic.cs
@@ -113,6 +113,42 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
         }
 
         [Fact]
+        public async Task ShouldMatchComprehensivePractitionerRolesWithMultipleIdentifierSystemsAsync()
+        {
+            // given
+            string inputSdsRoleProfileId = "999999000123-A1";
+
+            JsonElement source1Resource = CreateComprehensivePractitionerRoleResource(
+                sdsRoleProfileId: inputSdsRoleProfileId,
+                id: "practitioner-role-comprehensive-1");
+
+            JsonElement source2Resource = CreateComprehensivePractitionerRoleResource(
+                sdsRoleProfileId: inputSdsRoleProfileId,
+                id: "practitioner-role-comprehensive-2");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Matched.Add(
+                new MatchedResource(source1Resource, source2Resource, inputSdsRoleProfileId));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.practitionerRoleMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldExcludePractitionerRolesWithNoSdsRoleProfileIdFromMatchResultsAsync()
         {
             // given
