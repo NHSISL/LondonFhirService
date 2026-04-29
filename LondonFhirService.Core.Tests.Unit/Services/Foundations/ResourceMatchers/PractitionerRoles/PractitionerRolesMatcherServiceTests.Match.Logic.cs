9df01359d1d024ad7a520b0c24a2d57ab690e5c5
Raghu Peri <perirrs@gmail.com>
2026-04-29 19:38:05 +0000

    ShouldMatchPractitionerRolesWhenBothSourcesHaveResourcesWithSameKeyAsync -> FAIL

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/PractitionerRoles/PractitionerRolesMatcherServiceTests.Match.Logic.cs
@@ -0,0 +1,51 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.PractitionerRoles
+{
+    public partial class PractitionerRoleMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldMatchPractitionerRolesWhenBothSourcesHaveResourcesWithSameKeyAsync()
+        {
+            // given
+            string inputSdsRoleProfileId = "999999000123-A1";
+
+            JsonElement source1Resource = CreatePractitionerRoleResource(
+                sdsRoleProfileId: inputSdsRoleProfileId,
+                id: "practitioner-role-1");
+
+            JsonElement source2Resource = CreatePractitionerRoleResource(
+                sdsRoleProfileId: inputSdsRoleProfileId,
+                id: "practitioner-role-2");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Matched.Add(
+                new MatchedResource(source1Resource, source2Resource, inputSdsRoleProfileId));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.practitionerRoleMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
