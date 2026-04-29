2d7d089fd77b11b0d61069e75789e982d96a0b1a
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:30:45 +0000

    ShouldAddToUnmatchedFromSource1WhenOnlySource1HasMedicationRequestAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/MedicationRequests/MedicationRequestsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/MedicationRequests/MedicationRequestsMatcherServiceTests.Match.Logic.cs
@@ -47,5 +47,37 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldAddToUnmatchedFromSource1WhenOnlySource1HasMedicationRequestAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = "MR-1";
+
+            JsonElement source1Resource = CreateMedicationRequestResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "medication-request-1");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement>();
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Unmatched.Add(
+                new UnmatchedResource(source1Resource, "MedicationRequest", inputDdsIdentifierValue, true));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.medicationRequestMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
