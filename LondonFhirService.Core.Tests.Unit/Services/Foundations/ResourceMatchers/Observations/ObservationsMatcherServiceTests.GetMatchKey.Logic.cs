81fbb97461ca547ae4bb735492f365e1d02b4c23
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:36:44 +0000

    ShouldGetMatchKeyWhenObservationHasDdsIdentifierAsync -> PASS

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Observations/ObservationsMatcherServiceTests.GetMatchKey.Logic.cs
@@ -0,0 +1,37 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.Observations
+{
+    public partial class ObservationMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldGetMatchKeyWhenObservationHasDdsIdentifierAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = GetRandomDdsIdentifierValue();
+            string randomId = GetRandomString();
+
+            JsonElement observationResource = CreateObservationResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: randomId);
+
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+            string expectedMatchKey = inputDdsIdentifierValue;
+
+            // when
+            string actualMatchKey =
+                await this.observationMatcherService.GetMatchKeyAsync(observationResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().Be(expectedMatchKey);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
