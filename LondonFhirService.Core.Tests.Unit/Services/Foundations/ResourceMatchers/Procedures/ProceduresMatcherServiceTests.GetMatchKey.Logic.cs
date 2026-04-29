b6d2bf9dc1e360f487445f1d5b6bdf3491fc6fc3
Raghu Peri <perirrs@gmail.com>
2026-04-29 19:50:51 +0000

    ShouldReturnNullOnGetMatchKeyWhenProcedureHasNoIdentifierPropertyAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Procedures/ProceduresMatcherServiceTests.GetMatchKey.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Procedures/ProceduresMatcherServiceTests.GetMatchKey.Logic.cs
@@ -33,5 +33,25 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualMatchKey.Should().Be(expectedMatchKey);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldReturnNullOnGetMatchKeyWhenProcedureHasNoIdentifierPropertyAsync()
+        {
+            // given
+            string randomId = GetRandomString();
+
+            JsonElement procedureResource =
+                CreateProcedureResourceWithoutIdentifierProperty(id: randomId);
+
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+
+            // when
+            string actualMatchKey =
+                await this.procedureMatcherService.GetMatchKeyAsync(procedureResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().BeNull();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
