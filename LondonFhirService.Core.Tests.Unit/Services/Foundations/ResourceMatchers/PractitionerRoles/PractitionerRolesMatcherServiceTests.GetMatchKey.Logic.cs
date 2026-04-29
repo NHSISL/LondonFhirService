051b3402b414a3ee272e037bbb121099659f7c88
Raghu Peri <perirrs@gmail.com>
2026-04-29 19:39:34 +0000

    ShouldReturnNullOnGetMatchKeyWhenPractitionerRoleHasNoIdentifierPropertyAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/PractitionerRoles/PractitionerRolesMatcherServiceTests.GetMatchKey.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/PractitionerRoles/PractitionerRolesMatcherServiceTests.GetMatchKey.Logic.cs
@@ -33,5 +33,25 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualMatchKey.Should().Be(expectedMatchKey);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldReturnNullOnGetMatchKeyWhenPractitionerRoleHasNoIdentifierPropertyAsync()
+        {
+            // given
+            string randomId = GetRandomString();
+
+            JsonElement practitionerRoleResource =
+                CreatePractitionerRoleResourceWithoutIdentifierProperty(id: randomId);
+
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+
+            // when
+            string actualMatchKey =
+                await this.practitionerRoleMatcherService.GetMatchKeyAsync(practitionerRoleResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().BeNull();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
