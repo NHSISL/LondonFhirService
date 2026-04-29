5d91225ecba9b1d8227696329f06d0aa65283325
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:12:48 +0000

    ShouldMatchImmunizationsWhenBothSourcesHaveResourcesWithSameKeyAsync -> FAIL

---
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Immunizations/ImmunizationsMatcherServiceTests.Match.Logic.cs
@@ -0,0 +1,51 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LondonFhirService.Core.Models.Foundations.ResourceMatchers;
+
+namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatchers.Immunizations
+{
+    public partial class ImmunizationMatcherServiceTests
+    {
+        [Fact]
+        public async Task ShouldMatchImmunizationsWhenBothSourcesHaveResourcesWithSameKeyAsync()
+        {
+            // given
+            string inputDdsIdentifierValue = "IMM-1";
+
+            JsonElement source1Resource = CreateImmunizationResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "immunization-1");
+
+            JsonElement source2Resource = CreateImmunizationResource(
+                ddsIdentifierValue: inputDdsIdentifierValue,
+                id: "immunization-2");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Matched.Add(
+                new MatchedResource(source1Resource, source2Resource, inputDdsIdentifierValue));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.immunizationMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
