1f1e8e22c6ff847e32cf535c576abe08818ab7a4
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:26:30 +0000

    ShouldMatchComprehensiveLocationsWithMultipleIdentifierSystemsAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Locations/LocationsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Locations/LocationsMatcherServiceTests.Match.Logic.cs
@@ -113,6 +113,42 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
         }
 
         [Fact]
+        public async Task ShouldMatchComprehensiveLocationsWithMultipleIdentifierSystemsAsync()
+        {
+            // given
+            string inputOdsSiteCode = "A82817-1";
+
+            JsonElement source1Resource = CreateComprehensiveLocationResource(
+                odsSiteCode: inputOdsSiteCode,
+                id: "location-comprehensive-1");
+
+            JsonElement source2Resource = CreateComprehensiveLocationResource(
+                odsSiteCode: inputOdsSiteCode,
+                id: "location-comprehensive-2");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Matched.Add(
+                new MatchedResource(source1Resource, source2Resource, inputOdsSiteCode));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.locationMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldExcludeLocationsWithNoOdsSiteCodeFromMatchResultsAsync()
         {
             // given
