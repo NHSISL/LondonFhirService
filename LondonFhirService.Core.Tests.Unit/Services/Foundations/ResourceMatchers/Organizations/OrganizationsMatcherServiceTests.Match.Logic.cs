eac4710353ee5582523ed4deef3e2b7445e353cf
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:45:50 +0000

    ShouldMatchComprehensiveOrganizationsWithMultipleIdentifierSystemsAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Organizations/OrganizationsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Organizations/OrganizationsMatcherServiceTests.Match.Logic.cs
@@ -113,6 +113,42 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
         }
 
         [Fact]
+        public async Task ShouldMatchComprehensiveOrganizationsWithMultipleIdentifierSystemsAsync()
+        {
+            // given
+            string inputOdsOrganizationCode = "A82817";
+
+            JsonElement source1Resource = CreateComprehensiveOrganizationResource(
+                odsOrganizationCode: inputOdsOrganizationCode,
+                id: "organization-comprehensive-1");
+
+            JsonElement source2Resource = CreateComprehensiveOrganizationResource(
+                odsOrganizationCode: inputOdsOrganizationCode,
+                id: "organization-comprehensive-2");
+
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Matched.Add(
+                new MatchedResource(source1Resource, source2Resource, inputOdsOrganizationCode));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.organizationMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldExcludeOrganizationsWithNoOdsOrganizationCodeFromMatchResultsAsync()
         {
             // given
