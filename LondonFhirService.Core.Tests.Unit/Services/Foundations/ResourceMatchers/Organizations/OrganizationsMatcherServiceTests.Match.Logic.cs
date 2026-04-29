8bf0afcb0ec83215da7071ef7686aa5c5805d928
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:43:40 +0000

    ShouldAddToUnmatchedFromSource2WhenOnlySource2HasOrganizationAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Organizations/OrganizationsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Organizations/OrganizationsMatcherServiceTests.Match.Logic.cs
@@ -79,5 +79,37 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldAddToUnmatchedFromSource2WhenOnlySource2HasOrganizationAsync()
+        {
+            // given
+            string inputOdsOrganizationCode = "A82817";
+
+            JsonElement source2Resource = CreateOrganizationResource(
+                odsOrganizationCode: inputOdsOrganizationCode,
+                id: "organization-1");
+
+            var source1Resources = new List<JsonElement>();
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+
+            var expectedResourceMatch = new ResourceMatch();
+
+            expectedResourceMatch.Unmatched.Add(
+                new UnmatchedResource(source2Resource, "Organization", inputOdsOrganizationCode, false));
+
+            // when
+            ResourceMatch actualResourceMatch = await this.organizationMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
