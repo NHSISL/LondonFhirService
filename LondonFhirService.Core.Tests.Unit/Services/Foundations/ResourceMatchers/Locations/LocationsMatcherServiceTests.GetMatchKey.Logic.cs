8de72c32148c7185d3bd8b3f15c62831eff50a95
Raghu Peri <perirrs@gmail.com>
2026-04-29 16:25:14 +0000

    ShouldReturnNullOnGetMatchKeyWhenLocationHasNonOdsSiteCodeIdentifierAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Locations/LocationsMatcherServiceTests.GetMatchKey.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/Locations/LocationsMatcherServiceTests.GetMatchKey.Logic.cs
@@ -53,5 +53,22 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualMatchKey.Should().BeNull();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldReturnNullOnGetMatchKeyWhenLocationHasNonOdsSiteCodeIdentifierAsync()
+        {
+            // given
+            string randomId = GetRandomString();
+            JsonElement locationResource = CreateNonOdsLocationResource(id: randomId);
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+
+            // when
+            string actualMatchKey =
+                await this.locationMatcherService.GetMatchKeyAsync(locationResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().BeNull();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
