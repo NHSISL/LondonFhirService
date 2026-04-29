b75f7d24cfb9e776d43fd825550ace9ef2fdf166
Raghu Peri <perirrs@gmail.com>
2026-04-29 20:00:16 +0000

    ShouldExcludeProcedureRequestsWithNoDdsIdentifierFromMatchResultsAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ProcedureRequests/ProcedureRequestsMatcherServiceTests.Match.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/ProcedureRequests/ProcedureRequestsMatcherServiceTests.Match.Logic.cs
@@ -111,5 +111,29 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldExcludeProcedureRequestsWithNoDdsIdentifierFromMatchResultsAsync()
+        {
+            // given
+            JsonElement source1Resource = CreateNonDdsProcedureRequestResource(id: "procedure-request-1");
+            JsonElement source2Resource = CreateNonDdsProcedureRequestResource(id: "procedure-request-2");
+            var source1Resources = new List<JsonElement> { source1Resource };
+            var source2Resources = new List<JsonElement> { source2Resource };
+            Dictionary<string, JsonElement> source1ResourceIndex = CreateResourceIndex();
+            Dictionary<string, JsonElement> source2ResourceIndex = CreateResourceIndex();
+            var expectedResourceMatch = new ResourceMatch();
+
+            // when
+            ResourceMatch actualResourceMatch = await this.procedureRequestMatcherService.MatchAsync(
+                source1Resources,
+                source2Resources,
+                source1ResourceIndex,
+                source2ResourceIndex);
+
+            // then
+            actualResourceMatch.Should().BeEquivalentTo(expectedResourceMatch);
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
