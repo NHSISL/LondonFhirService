cf2475b65f13a5d92d4266d38e3a51b2ba0a8d7e
Raghu Peri <perirrs@gmail.com>
2026-04-29 20:20:54 +0000

    CODE RUB: Add comprehensive JSON 4.x smoke test for EpisodeOfCare matcher

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/EpisodeOfCare/EpisodeOfCareMatcherServiceTests.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/EpisodeOfCare/EpisodeOfCareMatcherServiceTests.cs
@@ -161,6 +161,109 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             return ParseJsonElement(json);
         }
 
+        private static JsonElement CreateComprehensiveEpisodeOfCareResource(
+            string periodStart,
+            string id)
+        {
+            string json = $$"""
+              {
+                "resourceType": "EpisodeOfCare",
+                "id": "{{id}}",
+                "meta": {
+                  "versionId": "1",
+                  "lastUpdated": "2024-09-12T09:25:00+01:00",
+                  "profile": [
+                    "https://fhir.hl7.org.uk/STU3/StructureDefinition/CareConnect-EpisodeOfCare-1"
+                  ]
+                },
+                "text": {
+                  "status": "generated",
+                  "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\"><p>Active episode of care for diabetes management.</p></div>"
+                },
+                "identifier": [
+                  {
+                    "use": "official",
+                    "system": "https://fhir.nhs.uk/Id/episode-of-care-id",
+                    "value": "EOC-1234c518"
+                  },
+                  {
+                    "use": "usual",
+                    "system": "https://fhir.hl7.org.uk/Id/dds",
+                    "value": "EOC-1234c518"
+                  }
+                ],
+                "status": "active",
+                "statusHistory": [
+                  {
+                    "status": "planned",
+                    "period": {
+                      "start": "2022-04-01",
+                      "end": "2022-04-12"
+                    }
+                  },
+                  {
+                    "status": "active",
+                    "period": {
+                      "start": "{{periodStart}}"
+                    }
+                  }
+                ],
+                "type": [
+                  {
+                    "coding": [
+                      {
+                        "system": "http://hl7.org/fhir/v3/episodeofcare-type",
+                        "code": "hacc",
+                        "display": "Home and Community Care"
+                      }
+                    ]
+                  }
+                ],
+                "diagnosis": [
+                  {
+                    "condition": {
+                      "reference": "Condition/c0a91a7d-8a49-4d20-93b1-3e6a0d29c0a1"
+                    },
+                    "role": {
+                      "coding": [
+                        {
+                          "system": "http://hl7.org/fhir/diagnosis-role",
+                          "code": "CC",
+                          "display": "Chief complaint"
+                        }
+                      ]
+                    },
+                    "rank": 1
+                  }
+                ],
+                "patient": {
+                  "reference": "Patient/6e6c53cb-fca3-4e34-9cbc-476c32f1eb3c"
+                },
+                "managingOrganization": {
+                  "reference": "Organization/56000299-bbd7-4dfa-ad64-c2d8692ae20c"
+                },
+                "period": {
+                  "start": "{{periodStart}}"
+                },
+                "referralRequest": [
+                  {
+                    "reference": "ReferralRequest/8f47d255-5e8d-4d3f-a4b1-d8c9b3e72211"
+                  }
+                ],
+                "careManager": {
+                  "reference": "Practitioner/8a77e616-2d12-4bc9-b0a1-2f1576ec1c05"
+                },
+                "team": [
+                  {
+                    "reference": "Organization/56000299-bbd7-4dfa-ad64-c2d8692ae20c"
+                  }
+                ]
+              }
+              """;
+
+            return ParseJsonElement(json);
+        }
+
         private static JsonElement ParseJsonElement(string json) =>
             JsonDocument.Parse(json).RootElement.Clone();
     }
