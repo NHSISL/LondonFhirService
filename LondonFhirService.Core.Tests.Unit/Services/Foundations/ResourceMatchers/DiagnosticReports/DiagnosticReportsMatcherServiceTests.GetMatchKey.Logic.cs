b963a1156535d5e0bc11093c11e4beceb348ef38
Raghu Peri <perirrs@gmail.com>
2026-04-29 15:13:34 +0000

    ShouldReturnNullOnGetMatchKeyWhenDiagnosticReportHasNoIdentifierPropertyAsync -> PASS

--- LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/DiagnosticReports/DiagnosticReportsMatcherServiceTests.GetMatchKey.Logic.cs
+++ LondonFhirService.Core.Tests.Unit/Services/Foundations/ResourceMatchers/DiagnosticReports/DiagnosticReportsMatcherServiceTests.GetMatchKey.Logic.cs
@@ -33,5 +33,25 @@ namespace LondonFhirService.Core.Tests.Unit.Services.Foundations.ResourceMatcher
             actualMatchKey.Should().Be(expectedMatchKey);
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldReturnNullOnGetMatchKeyWhenDiagnosticReportHasNoIdentifierPropertyAsync()
+        {
+            // given
+            string randomId = GetRandomString();
+
+            JsonElement diagnosticReportResource =
+                CreateDiagnosticReportResourceWithoutIdentifierProperty(id: randomId);
+
+            Dictionary<string, JsonElement> resourceIndex = CreateResourceIndex();
+
+            // when
+            string actualMatchKey =
+                await this.diagnosticReportMatcherService.GetMatchKeyAsync(diagnosticReportResource, resourceIndex);
+
+            // then
+            actualMatchKey.Should().BeNull();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
