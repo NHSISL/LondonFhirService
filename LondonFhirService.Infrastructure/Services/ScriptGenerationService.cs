23b5ca7743a327a1ff85d3df4555082bf6699ff1
Raghu <perirrs@gmail.com>
2026-06-30 15:49:41 +0100

    CODE RUB: Migrate pipeline components to latest versions and regenerate workflows

--- LondonFhirService.Infrastructure/Services/ScriptGenerationService.cs
+++ LondonFhirService.Infrastructure/Services/ScriptGenerationService.cs
@@ -7,7 +7,7 @@ using System.IO;
 using ADotNet.Clients;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
-using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV3s;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV5s;
 
 namespace LondonFhirService.Infrastructure.Services
 {
@@ -57,16 +57,16 @@ namespace LondonFhirService.Infrastructure.Services
                                     Run = "git config --system core.longpaths true"
                                 },
 
-                                new CheckoutTaskV3
+                                new CheckoutTaskV5
                                 {
                                     Name = "Check out"
                                 },
 
-                                new SetupDotNetTaskV3
+                                new SetupDotNetTaskV5
                                 {
                                     Name = "Setup .Net",
 
-                                    With = new TargetDotNetVersionV3
+                                    With = new TargetDotNetVersionV5
                                     {
                                         DotNetVersion = dotNetVersion
                                     }
@@ -125,7 +125,7 @@ namespace LondonFhirService.Infrastructure.Services
                     },
                     {
                         "add_tag",
-                        new TagJob(
+                        new TagJobV2(
                             runsOn: BuildMachines.UbuntuLatest,
                             dependsOn: "build",
                             projectRelativePath: $"{projectName}/{projectName}.csproj",
@@ -170,20 +170,14 @@ namespace LondonFhirService.Infrastructure.Services
                 {
                     {
                         "label",
-                        new LabelJobV2(runsOn: BuildMachines.UbuntuLatest)
+                        new LabelJobV3(runsOn: BuildMachines.UbuntuLatest)
                         {
                             Name = "Label",
-                            Permissions = new Dictionary<string, string>
-                            {
-                                { "contents", "read" },
-                                { "pull-requests", "write" },
-                                { "issues", "write" }
-                            }
                         }
                     },
                     {
                         "requireIssueOrTask",
-                        new RequireIssueOrTaskJob()
+                        new RequireIssueOrTaskJobV2(excludedAuthors: "dependabot[bot]")
                         {
                             Name = "Require Issue Or Task Association",
                         }
